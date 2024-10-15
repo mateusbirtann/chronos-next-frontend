4318411f8e5f1ce6b989a3968454cdece1d50a13
MateusBirtan <contatobirtan@gmail.com>
2024-10-15 17:52:04 -0300

    fix(ChatHeader): typo description fix

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -3,7 +3,7 @@ export function ChatHeader() {
     <>
       <h1 className="text-2xl font-bold text-left w-full">Chronos Frontend CEP Search</h1>
       <span className="text-sm text-left w-full">
-        Este é um projeto de frontend que consome a API do ViaCEP através do Assistent API da OpenAI.
+        Este é um projeto de frontend que consome a API do ViaCEP através do Assistant API da OpenAI.
       </span>
       <span className="text-sm text-left w-full">
         A aplicação permite ao usuário inserir um CEP e receber informações detalhadas sobre o endereço correspondente.
